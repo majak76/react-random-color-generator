b5d5c6d611fba376ffc187fb25428957846c6ac5
Maja Krmar <majakrmar05@gmail.com>
2023-05-03 16:00:05 +0200

    Modify the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ export default function App() {
   return (
     <div>
       <button type="button">Generate</button>
-
+      <div
+        style={{
+          border: 'solid 3px gray',
+          borderRadius: '50px',
+          padding: '5px',
+          backgroundColor: '#FFFF00',
+        }}
+      ></div>
       <div className="div">hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
     </div>
   );
 }
