3a30ca982c96d22a40de1bb760d16193817dae4e
Maja Krmar <majakrmar05@gmail.com>
2023-05-05 18:29:07 +0200

    Add new project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
+
 export default function App() {
+  // first create a state variable and then define it with a default value of a random color
+  const [color, setColor] = useState(randomColor().color);
+  const [colorName, setColorName] = useState('');
   return (
-    <div>
-      <button type="button">Generate</button>
-      <div
-        style={{
-          border: 'solid 3px gray',
-          borderRadius: '50px',
-          padding: '5px',
-          backgroundColor: '#FFFF00',
+    <div className="App" style={{ background: color }}>
+      {/*display the color on the screen*/}
+      {color}
+      <br />
+      <button
+        onClick={() => {
+          // Generate and set a new value for the color
+          const newColor = randomColor().color;
+          setColor(newColor);
         }}
-      ></div>
-      <div className="div">hello</div>
+      >
+        Generate
+      </button>
     </div>
   );
 }
