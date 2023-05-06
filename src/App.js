55a808573e52aa66f7c02b0a258d0c0e4b9997fa
Maja Krmar <majakrmar05@gmail.com>
2023-05-06 12:30:38 +0200

    Add the random color generator

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import { useState } from 'react';
 
 export default function App() {
   // first create a state variable and then define it with a default value of a random color
-  const [color, setColor] = useState(randomColor().color);
-  const [colorName, setColorName] = useState('');
+  const [color, setColor] = useState(randomColor);
   return (
-    <div className="App" style={{ background: color }}>
-      {/*display the color on the screen*/}
-      {color}
-      <br />
-      <button
-        onClick={() => {
-          // Generate and set a new value for the color
-          const newColor = randomColor().color;
-          setColor(newColor);
-        }}
-      >
-        Generate
-      </button>
+    <div className="container">
+      <div className="box" style={{ background: color }}>
+        Generated Color:
+        <br />
+        {color}
+        <br />
+        <button
+          onClick={() =>
+            // Generate and set a new value for the color
+            setColor(randomColor)
+          }
+        >
+          Generate
+        </button>
+      </div>
     </div>
   );
 }
